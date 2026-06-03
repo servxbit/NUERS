df5d6cade462fdc4e6de05ca80d97c5708bd6cd7
servxbit <servxbit@gmail.com>
2026-06-03 23:53:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    return 'Cache cleared';
+});
+
 Route::view('/{path?}', 'app')->where('path', '^(?!api).*$');
+
+
